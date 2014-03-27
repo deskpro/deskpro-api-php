8f31afb5bf64411db418ad3d0046b549ba58644c
Abhinav Kumar <abhinavkumar940@gmail.com>
2014-03-27 19:41:59 +0530

    Tasks request calls moved to Task Service

--- src/Service/Tasks.php
+++ src/Service/Tasks.php
@@ -12,226 +12,232 @@ namespace DeskPRO\Service;
 class Tasks extends AbstractService
 {
 
-    /**
-     * Creates and returns a search criteria
-     * 
-     * @return \DeskPRO\Criteria\Task
-     */
-    public function createCriteria()
-    {
-        return new \DeskPRO\Criteria\Task();
-    }
+	/**
+	 * Creates and returns a search criteria
+	 * 
+	 * @return \DeskPRO\Criteria\Task
+	 */
+	public function createCriteria()
+	{
+		return new \DeskPRO\Criteria\Task();
+	}
 
-    /**
-     * Creates and returns a TaskBuilder object
-     * 
-     * @return \DeskPRO\Builder\Task
-     */
-    public function createTaskEditor()
-    {
-        return new \DeskPRO\Builder\Task();
-    }
+	/**
+	 * Creates and returns a TaskBuilder object
+	 * 
+	 * @return \DeskPRO\Builder\Task
+	 */
+	public function createTaskEditor()
+	{
+		return new \DeskPRO\Builder\Task();
+	}
 
-    /**
-     * Finds tasks matching the criteria
-     * 
-     * @param \DeskPRO\Criteria\Task $criteria
-     * @return \DpApiResult
-     */
-    public function find(\DeskPRO\Criteria\Task $criteria)
-    {
-        return $this->interface->findTasks($criteria->toArray());
-    }
+	/**
+	 * Finds tasks matching the criteria
+	 * 
+	 * @param \DeskPRO\Criteria\Task $criteria
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function find(\DeskPRO\Criteria\Task $criteria)
+	{
+		return $this->call('GET', '/tasks', $criteria->toArray());
+	}
 
-    /**
-     * Gets an Task by task ID
-     * 
-     * @param int $taskId
-     * @return \DpApiResult Result Object
-     */
-    public function findById($taskId)
-    {
-        return $this->interface->getTask($taskId);
-    }
+	/**
+	 * Gets an Task by task ID
+	 * 
+	 * @param int $taskId
+	 * @return \DeskPRO\Api\Result Result Object
+	 */
+	public function findById($taskId)
+	{
+		return $this->call('GET', '/tasks/' . intval($id));
+	}
 
-    /**
-     * Saves a task
-     * 
-     * @param \DeskPRO\Builder\Task $task
-     * @return \DpApiResult
-     */
-    public function save(\DeskPRO\Builder\Task $task)
-    {
-        if ($task->getId()) {
-            return $this->interface->updateTask($task->getId(), $task->getDataArray());
-        }
+	/**
+	 * Saves a task
+	 * 
+	 * @param \DeskPRO\Builder\Task $task
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function save(\DeskPRO\Builder\Task $task)
+	{
+		if ($task->getId()) {
+			return $this->call('POST', '/tasks/' . intval($task->getId()), $task->getDataArray());
+		}
 
-        return $this->interface->createTask($task->getDataArray());
-    }
+		return $this->call('POST', '/tasks', $task->getDataArray());
+	}
 
-    /**
-     * Deletes the given task.
-     *
-     * @param int $taskId
-     *
-     * @return \DpApiResult
-     */
-    public function deleteTask($taskId)
-    {
-        return $this->interface->deleteTask($taskId);
-    }
+	/**
+	 * Deletes the given task.
+	 *
+	 * @param int $taskId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function deleteTask($taskId)
+	{
+		return $this->call('DELETE', '/task/' . intval($taskId));
+	}
 
-    /**
-     * Gets all associations for a task
-     *
-     * @param int $taskId
-     *
-     * @return \DpApiResult
-     */
-    public function getTaskAssociations($taskId)
-    {
-        return $this->interface->getTaskAssociations($taskId);
-    }
+	/**
+	 * Gets all associations for a task
+	 *
+	 * @param int $taskId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function getTaskAssociations($taskId)
+	{
+		return $this->call('GET', '/tasks/' . intval($taskId) . '/associations');
+	}
 
-    /**
-     * Adds an association for a task.
-     *
-     * @param int $taskId Task ID
-     * @param int $ticketId Ticket ID to associate the task to
-     *
-     * @return \DpApiResult
-     */
-    public function addTaskAssociation($taskId, $ticketId)
-    {
-        return $this->interface->addTaskAssociation($taskId, $ticketId);
-    }
+	/**
+	 * Adds an association for a task.
+	 *
+	 * @param int $taskId Task ID
+	 * @param int $ticketId Ticket ID to associate the task to
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function addTaskAssociation($taskId, $ticketId)
+	{
+		$params = array(
+		    'ticket_id' => $ticketId
+		);
+		return $this->call('POST', '/tasks/' . intval($taskId) . '/associations', $params);
+	}
 
-    /**
-     * Determines if a particular association ID exists for a task.
-     *
-     * @param int $taskId
-     * @param int $associationId
-     *
-     * @return \DpApiResult
-     */
-    public function getTaskAssociation($taskId, $associationId)
-    {
-        return $this->interface->getTaskAssociation($taskId, $associationId);
-    }
+	/**
+	 * Determines if a particular association ID exists for a task.
+	 *
+	 * @param int $taskId
+	 * @param int $associationId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function getTaskAssociation($taskId, $associationId)
+	{
+		return $this->call('GET', '/tasks/' . intval($taskId) . '/associations/' . intval($associationId));
+	}
 
-    /**
-     * Deletes a particular task association
-     *
-     * @param int $taskId
-     * @param int $associationId
-     *
-     * @return \DpApiResult
-     */
-    public function deleteTaskAssociation($taskId, $associationId)
-    {
-        return $this->interface->deleteTaskAssociation($taskId, $associationId);
-    }
+	/**
+	 * Deletes a particular task association
+	 *
+	 * @param int $taskId
+	 * @param int $associationId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function deleteTaskAssociation($taskId, $associationId)
+	{
+		return $this->call('DELETE', '/tasks/' . intval($taskId) . '/associations/' . intval($associationId));
+	}
 
-    /**
-     * Gets all comments for a task
-     *
-     * @param int $taskId
-     *
-     * @return \DpApiResult
-     */
-    public function getTaskComments($taskId)
-    {
-        return $this->interface->getTaskComments($taskId);
-    }
+	/**
+	 * Gets all comments for a task
+	 *
+	 * @param int $taskId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function getTaskComments($taskId)
+	{
+		return $this->call('GET', '/tasks/' . intval($taskId) . '/comments');
+	}
 
-    /**
-     * Adds a comment for a task.
-     *
-     * @param int $taskId
-     * @param string $comment
-     *
-     * @return \DpApiResult
-     */
-    public function addTaskComment($taskId, $comment)
-    {
-        return $this->interface->addTaskComment($taskId, $comment);
-    }
+	/**
+	 * Adds a comment for a task.
+	 *
+	 * @param int $taskId
+	 * @param string $comment
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function addTaskComment($taskId, $comment)
+	{
+		$params = array(
+		    'comment' => $comment
+		);
+		return $this->call('POST', '/tasks/' . intval($taskId) . '/comments', $params);
+	}
 
-    /**
-     * Determines if a particular comment ID exists for a task.
-     *
-     * @param int $taskId
-     * @param int $commentId
-     *
-     * @return \DpApiResult
-     */
-    public function getTaskComment($taskId, $commentId)
-    {
-        return $this->interface->getTaskComment($taskId, $commentId);
-    }
+	/**
+	 * Determines if a particular comment ID exists for a task.
+	 *
+	 * @param int $taskId
+	 * @param int $commentId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function getTaskComment($taskId, $commentId)
+	{
+		return $this->call('GET', '/tasks/' . intval($taskId) . '/comments/' . intval($commentId));
+	}
 
-    /**
-     * Deletes a particular task comment
-     *
-     * @param int $taskId
-     * @param int $commentId
-     *
-     * @return \DpApiResult
-     */
-    public function deleteTaskComment($taskId, $commentId)
-    {
-        return $this->interface->deleteTaskComment($taskId, $commentId);
-    }
+	/**
+	 * Deletes a particular task comment
+	 *
+	 * @param int $taskId
+	 * @param int $commentId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function deleteTaskComment($taskId, $commentId)
+	{
+		return $this->call('DELETE', '/tasks/' . intval($taskId) . '/comments/' . intval($commentId));
+	}
 
-    /**
-     * Gets all labels associated with a task.
-     *
-     * @param int $taskId
-     *
-     * @return \DpApiResult
-     */
-    public function getTaskLabels($taskId)
-    {
-        return $this->interface->getTaskLabels($taskId);
-    }
+	/**
+	 * Gets all labels associated with a task.
+	 *
+	 * @param int $taskId
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function getTaskLabels($taskId)
+	{
+		return $this->call('GET', '/tasks/' . intval($id) . '/labels');
+	}
 
-    /**
-     * Adds a label to a task.
-     *
-     * @param int $taskId
-     * @param string $label
-     *
-     * @return \DpApiResult
-     */
-    public function addTaskLabel($taskId, $label)
-    {
-        return $this->interface->addTaskLabel($taskId, $label);
-    }
+	/**
+	 * Adds a label to a task.
+	 *
+	 * @param int $taskId
+	 * @param string $label
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function addTaskLabel($taskId, $label)
+	{
+		return $this->call('POST', '/tasks/' . intval($taskId) . '/labels', array('label' => $label));
+	}
 
-    /**
-     * Determines if a task has a specific label.
-     *
-     * @param int $taskId
-     * @param string $label
-     *
-     * @return \DpApiResult
-     */
-    public function getTaskLabel($taskId, $label)
-    {
-        return $this->interface->getTaskLabel($taskId, $label);
-    }
+	/**
+	 * Determines if a task has a specific label.
+	 *
+	 * @param int $taskId
+	 * @param string $label
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function getTaskLabel($taskId, $label)
+	{
+		return $this->call('GET', '/tasks/' . intval($taskId) . '/labels/' . $label);
+	}
 
-    /**
-     * Removes a label from a task.
-     *
-     * @param int $taskId
-     * @param string $label
-     *
-     * @return \DpApiResult
-     */
-    public function removeTaskLabel($taskId, $label)
-    {
-        return $this->interface->removeTaskLabel($taskId, $label);
-    }
+	/**
+	 * Removes a label from a task.
+	 *
+	 * @param int $taskId
+	 * @param string $label
+	 *
+	 * @return \DeskPRO\Api\Result
+	 */
+	public function removeTaskLabel($taskId, $label)
+	{
+		return $this->call('DELETE', '/tasks/' . intval($taskId) . '/labels/' . $label);
+	}
 
 }
