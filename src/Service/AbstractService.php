793eab192e79f4c1ffae344e7b66c3586044d2ac
Abhinav Kumar <abhinavkumar940@gmail.com>
2014-04-01 12:21:20 +0530

    FileUpload related functions added

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -51,4 +51,57 @@ class AbstractService
 		return $this->interface->call($method, $end, $params);
 	}
 
+	/**
+	 * Ensures that the input is a file upload. If it's a string,
+	 * it is assumed to be a local file.
+	 *
+	 * @param mixed $input
+	 *
+	 * @return DpApiFileUpload
+	 *
+	 * @throws DpApiException
+	 */
+	protected function _enforceFileUpload($input)
+	{
+		if ($input instanceof DpApiFileUpload) {
+			return $input;
+		} else if (is_scalar($input)) {
+			return new DpApiFileUpload($input);
+		} else {
+			throw new DpApiException('Cannot force an input to a file (expected string or DpApiFileUpload object)');
+		}
+	}
+
+	/**
+	 * Ensures that a particular parameter is a file upload, if it is set.
+	 *
+	 * @param array $params
+	 * @param string $key
+	 * @param bool $multiple
+	 *
+	 * @return array
+	 *
+	 * @throws DpApiException
+	 */
+	protected function _enforceFileUploadIsset(array $params, $key, $multiple = false)
+	{
+		if (!isset($params[$key])) {
+			return $params;
+		}
+
+		if (is_array($params[$key])) {
+			if ($multiple) {
+				foreach ($params[$key] AS &$value) {
+					$value = $this->_enforceFileUpload($value);
+				}
+			} else {
+				throw new DpApiException("Passed multiple files to $key when only one was expected");
+			}
+		} else {
+			$params[$key] = $this->_enforceFileUpload($params[$key]);
+		}
+
+		return $params;
+	}
+
 }
